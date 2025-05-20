682a7d2c8461ab5efd9bd2e15885185533b8805c
kaviyarasan2210 <kaviyarasan.t@twilightitsolutions.com>
2025-05-20 11:52:52 +0530

    fix: env code commented

--- applications/dataset/cmd/main.go
+++ applications/dataset/cmd/main.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strconv"
 	"time"
 
 	"github.com/PecozQ/aimx-library/common"
-	"github.com/joho/godotenv"
 	base "whatsdare.com/fullstack/aimx/backend"
 	"whatsdare.com/fullstack/aimx/backend/service"
 	"whatsdare.com/fullstack/aimx/backend/worker"
@@ -23,21 +20,21 @@ func main() {
 	// Start the dataset path worker (processes file paths and sends chunks)
 	go worker.StartDatasetPathWorker()
 
-	// Get the current working directory (from where the command is run)
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal("Error getting current working directory:", err)
-	}
-	fmt.Println("Current Working Directory:", dir)
+	// // Get the current working directory (from where the command is run)
+	// dir, err := os.Getwd()
+	// if err != nil {
+	// 	log.Fatal("Error getting current working directory:", err)
+	// }
+	// fmt.Println("Current Working Directory:", dir)
 
-	// Construct the path to the .env file in the root directory
-	envPath := filepath.Join(dir, "./.env")
+	// // Construct the path to the .env file in the root directory
+	// envPath := filepath.Join(dir, "./.env")
 
-	// Load the .env file from the correct path
-	err = godotenv.Load(envPath)
-	if err != nil {
-		log.Fatal("Error loading .env file", err)
-	}
+	// // Load the .env file from the correct path
+	// err = godotenv.Load(envPath)
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file", err)
+	// }
 
 	// Create HTTP handlers
 	httpHandlers := base.MakeHttpHandler(s)
@@ -52,7 +49,7 @@ func main() {
 	fmt.Println("Info", "HTTP server started", "port", 8084)
 
 	// Start HTTP server
-	err = httpServer.ListenAndServe()
+	err := httpServer.ListenAndServe()
 	if err != nil {
 		log.Fatalf("HTTP server failed: %v", err)
 	}
