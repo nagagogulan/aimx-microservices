922ed13e101ab4cff3983459f2da6b574c9f9b93
kaviyarasan2210 <kaviyarasan.t@twilightitsolutions.com>
2025-05-21 21:07:55 +0530

    fix: test api added for system & env variable changes

--- applications/system/cmd/main.go
+++ applications/system/cmd/main.go
@@ -45,27 +45,41 @@ import (
 // }
 
 func main() {
+	// DB, err := pgsql.InitDB(&pgsql.Config{
+	// 	// my local host
+	// 	DBHost:     "13.229.196.7",
+	// 	DBPort:     5432,
+	// 	DBUser:     "myappuser",
+	// 	DBPassword: "SmartWork@123",
+	// 	DBName:     "aimxdb",
+
+	// 	// rds
+	// 	// DBHost:     "localhost",
+	// 	// DBPort:     5432,
+	// 	// DBUser:     "postgres",
+	// 	// DBPassword: "password@123",
+	// 	// DBName:     "localDb",
+
+	// 	// build dev
+	// 	// DBHost:     "localhost",
+	// 	// DBPort:     5432,
+	// 	// DBUser:     "postgres",
+	// 	// DBPassword: "Admin",
+	// 	// DBName:     "mylocaldb",
+	// })
+
+	dbPortStr := os.Getenv("DBPORT")
+	dbPort, err := strconv.Atoi(dbPortStr)
+	if err != nil {
+		fmt.Printf("Invalid DBPORT value: %v\n", err)
+		return
+	}
 	DB, err := pgsql.InitDB(&pgsql.Config{
-		// my local host
-		DBHost:     "13.229.196.7",
-		DBPort:     5432,
-		DBUser:     "myappuser",
-		DBPassword: "SmartWork@123",
-		DBName:     "aimxdb",
-
-		// rds
-		// DBHost:     "localhost",
-		// DBPort:     5432,
-		// DBUser:     "postgres",
-		// DBPassword: "password@123",
-		// DBName:     "localDb",
-
-		// build dev
-		// DBHost:     "localhost",
-		// DBPort:     5432,
-		// DBUser:     "postgres",
-		// DBPassword: "Admin",
-		// DBName:     "mylocaldb",
+		DBHost:     os.Getenv("DBHOST"),
+		DBPort:     dbPort,
+		DBUser:     os.Getenv("DBUSER"),
+		DBPassword: os.Getenv("DBPASSWORD"),
+		DBName:     os.Getenv("DBNAME"),
 	})
 	if err != nil {
 		log.Fatalf("Error initializing DB: %v", err)
